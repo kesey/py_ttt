86f9c02bb4c1e1e81b8657b220028aefa6592fc8
FabienMeunier <meunier.fabien@yahoo.fr>
2023-04-27 19:16:57 +0200

    add fixed position menu

---
+++ static/js/ttt_front.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $(window).scroll(function (event) { // keep menu on top
+        var scroll = $(window).scrollTop();
+        if (scroll > 145) {
+            $(".nav_menu ul").css({
+                "position": "fixed",
+                "top": "0px",
+                "background-color": "rgba(0, 0, 0, 0.75)",
+                "border-style": "double",
+                "border-color": "white",
+                "border-radius": "5px"
+            });
+        } else {
+            $(".nav_menu ul").css({
+                "position": "static",
+                "background-color": "black",
+                "border-style": "none"
+            });
+        }
+    });
+});
