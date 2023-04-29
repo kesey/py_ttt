99ec507ed7923f9e6d5db61be007bf70cc05212f
FabienMeunier <meunier.fabien@yahoo.fr>
2023-04-29 02:48:35 +0200

    delete key

--- ttt_front/templatetags/ttt_front_extras.py
+++ ttt_front/templatetags/ttt_front_extras.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def value_from_key(dictionnary:dict, key):
     return dictionnary[key]
+
+@register.filter
+def addf(value, arg):
+    return float(value) + float(arg)
